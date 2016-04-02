913796b70c1cfc6147858fda7df9a674402f490d
Henrique Rodrigues <henrique@localhost.localdomain>
2016-04-02 12:42:54 -0300

    Error page done

--- public/functions.js
+++ public/functions.js
@@ -1,13 +1,27 @@
 var works = {
+	intro: "pages/intro.html",
 	inventario: "pages/inventario.html",
 	racetimer: "pages/racetimer.html",
-	cerberus: "pages/cerberus.html"
+	cerberus: "pages/cerberus.html",
+	error: "pages/noFileStatus.html"
 };
 
 function load(page) {
 	for (var project in works) {
 		if (project === page) {
-			document.getElementById("details").src = works[project];
+			var dir = works[project];
+
+			var http = new XMLHttpRequest();
+			http.onreadystatechange = function() {
+				if (http.readyState == 4 && http.status == 200) {
+					document.getElementById("details").src = dir;
+				} else {
+					document.getElementById("details").src = works["error"];
+				}
+			}
+			http.open('HEAD', dir);
+			http.send();
+
 			break;
 		}
 	}
